d3648abca5a9b8e9b031b3c19cab4218b6f3586b
Tony <emperialace@gmail.com>
2025-01-26 00:14:19 -0500

    Created table for book data

--- library.js
+++ library.js
@@ -16,18 +16,50 @@ function Book(title, author,pages,read)
 function addBookToLibrary(title, author,pages,read){
 
     let book = new Book(title, author,pages,read);
+    console.log(book.title);
     myLibrary.push(book);
-    document.getElementById("addBook").style.display="none";
-    alert("Added "  + title)
+    //document.getElementById("addBook").style.display="none";
+    //alert("Added "  + title)
+    console.log(myLibrary);
     ;
 
 }
-
 
 function openForm()
 {
     document.getElementById("addBook").style.display="block";
 }
+
+
+ //document.getElementById("btnOpen").addEventListener('click', openForm);
+
+
+
+
+function displayBooks()
+{
+    <table style= "width: 100%">
+        <tr>
+            <th>Title</th>
+            <th>
+                Author
+            </th>
+            <th>
+                Pages
+            </th>
+            <th>
+                READ? 
+            </th>
+        </tr>
+
+    </table>
+};
+
+document.querySelector("main").innerHTML = `
+<ol>
+    ${displayBooks()}
+</ol>`;
+
 
 function closeForm()
 {
